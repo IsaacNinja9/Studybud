bc7cd586c8cf7a18904004bdeebf2f4906d0b005
IsaacNinja9 <isaacm9798@gmail.com>
2025-03-09 22:21:08 -0400

    User Profile Page

--- manage.py
+++ manage.py
@@ -6,7 +6,6 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    print(os)
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studybud.settings')
     try:
         from django.core.management import execute_from_command_line
